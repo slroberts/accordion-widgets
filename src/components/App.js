c7aa0a0a1eb6a2ec2d4b31e62352206ff7079f1b
Shomari Roberts <shomariroberts@gmail.com>
2021-04-08 20:28:27 -0400

    created and wired up Search component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Accordion from './Accordion';
+import Search from './Search';
 
 const items = [
   {
@@ -20,6 +21,7 @@ const App = () => {
   return (
     <div>
       <Accordion items={items} />
+      <Search />
     </div>
   );
 };
