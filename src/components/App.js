6875760f689b839e9fcbbc9aa68a32e9f10dc337
Shomari Roberts <shomariroberts@gmail.com>
2021-05-24 16:06:49 -0400

    added event handlers for dropdown actions

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from './Accordion';
 import Dropdown from './Dropdown';
 import Search from './Search';
@@ -34,11 +34,17 @@ const options = [
 ];
 
 const App = () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
       <Accordion items={items} />
       <Search />
-      <Dropdown options={options} />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
