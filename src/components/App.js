cd59c440c01afc05c8f3536c86c98d610829d087
Shomari Roberts <shomariroberts@gmail.com>
2021-06-09 22:39:18 -0400

    fixed bug with links that made unnecessary  request

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import Dropdown from './Dropdown';
 import Search from './Search';
 import Translate from './Translate';
 import Route from './Route';
+import Header from './Header';
 
 const items = [
   {
@@ -40,6 +41,7 @@ const App = () => {
 
   return (
     <div>
+      <Header />
       <Route path="/">
         <Accordion items={items} />
       </Route>
